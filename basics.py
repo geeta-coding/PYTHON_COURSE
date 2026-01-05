e783d51c7fe51c3dc5eb8fcd9fc16244ea0a087d
geeta <geeta@gmail.com>
2026-01-05 19:20:14 +0530

    Added the contain

---
+++ basics.py
@@ -0,0 +1,57 @@
+# Python is a high-level, interpreted programming language known for its simplicity and readability.
+# Real-World Use Cases
+# 1. Web Development: Frameworks like Django and Flask are used to build web applications.
+# 2. Data Science: Libraries such as Pandas, NumPy, and Matplotlib are widely used for data analysis and visualization.
+# 3. Machine Learning: TensorFlow and Scikit-learn are popular libraries for building
+#    machine learning models.
+# 4. Automation: Python scripts are often used to automate repetitive tasks.
+# 5. Scripting: Python is commonly used for writing small scripts to perform various tasks.
+# 6. Game Development: Libraries like Pygame are used for developing simple games.
+
+print("Basics of Python ")
+# This is a simple Python program that prints a message to the console.
+print("Hello,world!")
+# The print() function is used to output text to the console.
+
+comment = "This is a comment in Python"
+# Comments are used to explain code and are ignored by the Python interpreter.
+# In Python, comments start with the '#' symbol.
+# They can be placed on their own line or at the end of a line of code.
+print(comment)
+
+# your task is to learn and practice these basics to build a strong foundation in Python programming.
+print("YOUR NAME ")
+print("YOUR ROLL NUMBER ")
+# Learning these basics is essential for anyone starting with Python programming.
+print("geeta raut")
+print("209394")
+
+# Understanding these fundamentals will help you write efficient and effective Python code.
+# initialization of variables in python is very simple. You just need to assign a value to a variable name using the equal sign (=). dont need to declare 
+# the data type of the variable
+name = "geeta"
+job = 'student'
+bio = """I am a student learning Python programming."""
+age = 20
+
+print(name,job,bio,age)
+# in python ,you use a "" , '' or """ """ to define string variables.
+# Triple quotes (""" or ''') are used for multi-line strings or docstrings.
+print("\n Data Types in Python :")
+# Python has several built-in data types, including:
+# 1. int: Integer type, used to represent whole numbers.
+num1 = 10
+print("\ninterger data type:",num1)
+num2 = 2.0
+print("\nfloat data type:",num2)
+# 2. float: Floating-point type, used to represent decimal numbers.
+# wecan check it using type() function
+print("\nType of num1:",type(num1))
+print("\nType of num2:",type(num2))
+
+# also we can find the memory address of a variable using id() function 
+print(id(num1))
+print(id(num2))
+
+# now you try it yourself
+
